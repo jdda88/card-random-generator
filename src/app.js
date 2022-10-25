89dff5600a14a0019128a9aa96c5f3c7f1ca6ad7
Juan David <jdda886@gmail.com>
2022-10-25 22:40:07 +0000

    Completed exercise

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let generatedSuit = generateRandomSuit();
+  if (generatedSuit == "♦" || generatedSuit == "♥") {
+    document.querySelector(".card").style.color = "red";
+  }
+  document.querySelector(".top-suit").innerHTML = generatedSuit;
+  document.querySelector(".bottom-suit").innerHTML = generatedSuit;
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+let generateRandomNumber = () => {
+  let numAvailable = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let num = Math.floor(Math.random() * numAvailable.length);
+  return numAvailable[num];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
